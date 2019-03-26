0e5d510bf48125e1aa97e8e4a3c5647092a77a5e
jwj <wook3210@gmail.com>
2019-03-26 23:28:55 +0900

    jdbcTemplate 이용하기   + SqlParameterSource ( Map, BeanProperty .. )   + 람다, RowMapper

--- src/main/java/com/example/service/CustomerService.java
+++ src/main/java/com/example/service/CustomerService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.domain.Customer;
-import com.example.domain.CustomerRepository;
+import com.example.repository.CustomerRepository;
 
 
 
